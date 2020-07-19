f782b7e9b483ef768aa78f59a1c44599a7d6405c
Stanley <angstanley68@gmail.com>
2020-07-19 17:47:14 +0700

    feat: add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
       },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       {
         rel: 'stylesheet',
         href:
